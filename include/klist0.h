c83430299811eacaab39ab631ce3a0030ff9cd09
Alexander Shishkin <alexander.shishkin@siemens.com>
2006-10-11 17:01:08 +0400

    Linux-alike linked list implementation.

---
+++ include/klist0.h
@@ -0,0 +1,89 @@
+
+/*
+ * include/klist0.h 
+ *
+ * Copyright (C) 2006 Alexander Shishkin
+ *
+ * Is distributed under the GPL 2 license.
+ *
+ * Linux-alike doubly-linked list implementation, take 1
+ * "full intrusion".
+ *
+ * Abstract. Apparently any kind of less intrusive list implementation
+ * (as in klist.c+klist.h) is not quite suitable for:
+ *   a) having the same object linked in more than one list (which is handy
+ *      for many types of objects, including threads)
+ *   b) linking objects that are allocated differently and/or separately
+ *      compared to normal list objects
+ *   c) linking static objects.
+ * Intrusive lists are also much nicer with respect to cpu cache lines.
+ * This is an attempt at implementation of an idea of linked lists as seen
+ * in linux (include/linux/list.h).
+ * Note1: this is not a copy of that in linux so be prepared for nasty bugs.
+ *
+ * Note2: a list should never ever contain invalid pointers as long as it
+ *   exists (namely NULL, list poison etc). Instead, an empty list is a node
+ *   linked to itselfs at both ends.
+ */
+
+#ifndef __KLIST0_H__
+#define __KLIST0_H__
+
+/*
+ * Simple as ever. This structure gets embedded into node objects, one per
+ * list.
+ */
+struct klist0_node {
+	struct klist0_node *prev, *next;
+};
+
+#define KLIST0_DECLARE(list) \
+	struct klist0_node list = { &(list), &(list) };
+
+#define KLIST0_INIT(list) \
+	do { (list)->next = (list)->prev = (list); } while (0);
+
+/*
+ * Add a node before the given one into the list
+ * @node -- node to be added
+ * @list -- node of the list
+ */
+#define klist0_prepend(node, list) \
+	do { \
+		(node)->next = (list);	 \
+		(node)->prev = (list)->prev; \
+		(list)->prev->next = (node); \
+		(list)->prev = (node); \
+	} while (0);
+
+/*
+ * Add a node after the given one into the list
+ * @node -- node to be added
+ * @list -- node of the list
+ */
+#define klist0_append(node, list) \
+	do { \
+		(node)->next = (list)->next; \
+		(node)->prev = (list); \
+		(list)->next->prev = (node); \
+		(list)->next = (node); \
+	} while (0);
+
+/*
+ * Remove a node from it's list and relink it so that it be included
+ * in a valid empty list, as we're always consistent
+ * @node -- node to be removed
+ * Has no effect on an empty list: does no harm, but makes no sense.
+ */
+#define klist0_unlink(node) \
+	do { \
+		(node)->prev->next = (node)->next; \
+		(node)->next->prev = (node)->prev; \
+		(node)->next = (node); \
+		(node)->prev = (node); \
+	} while (0);
+
+#define klist0_entry(node, type, list) \
+	( (type *)((char *)node - offsetof(type, list)) )
+
+#endif
---
+++ tests/klist0-unit.c
@@ -0,0 +1,137 @@
+/*
+ * unit test for linked list implementation
+ * klist0.h
+ *
+ * interfaces description
+ * + klist0_prepend
+ * + klist0_append
+ * + klist0_unlink
+ * + klist0_entry
+ *
+ * P.S. We'll turn this into a testing framework one day.
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "klist0.h"
+
+/* ------------------------------------------ */
+/* generic test stuff                         */
+/* ------------------------------------------ */
+static unsigned int TESTS_FAILED = 0;
+static unsigned int TESTS_PASSED = 0;
+#define TEST_FAILURE(n) \
+	do { \
+		TESTS_FAILED++; \
+		printf("TEST %d failed\n", n); \
+	} while (0)
+
+#define TEST_PASSED(n) \
+	do { \
+		TESTS_PASSED++; \
+		printf("TEST %d passed\n", n); \
+	} while (0)
+
+#define TEST_DECLARE(n, title) \
+	printf("TEST %d, \"%s\"\n", n, title)
+
+#define dump_node(name, node) \
+	do { \
+		printf("%s: %p, %s->prev: %p, %s->next: %p\n", \
+			name, &node, name, node.prev, name, node.next); \
+	} while (0);
+#define separator() \
+	printf("------------------------\n");
+/* ------------------------------------------ */
+/* end of generic test stuff                  */
+/* ------------------------------------------ */
+
+#define MAX_NODES 4
+typedef struct list_test {
+	struct klist0_node the_list;
+	int data;
+} list_test_struct;
+#define TEST_KEY 1234
+
+struct list_test _test;
+struct list_test _node[MAX_NODES];
+
+int main() {
+	int i;
+
+	/* test 1: klist0_append() */
+	TEST_DECLARE(1, "klist0_append()");
+	KLIST0_INIT(&_test.the_list);
+	_test.data = TEST_KEY;
+	for (i = 0; i < MAX_NODES; i++) {
+		KLIST0_INIT(&_node[i].the_list);
+		_node[i].data = i;
+	}
+
+	klist0_append(&_node[1].the_list, &_node[0].the_list);
+	klist0_append(&_node[2].the_list, &_node[1].the_list);
+	dump_node("_node[0]", _node[0].the_list);
+	dump_node("_node[1]", _node[1].the_list);
+	dump_node("_node[2]", _node[2].the_list);
+	if (_node[0].the_list.next != &_node[1].the_list ||
+	    _node[1].the_list.next != &_node[2].the_list ||
+	    _node[2].the_list.next != &_node[0].the_list ||
+	    _node[0].the_list.prev != &_node[2].the_list ||
+	    _node[1].the_list.prev != &_node[0].the_list ||
+	    _node[2].the_list.prev != &_node[1].the_list)
+		TEST_FAILURE(1);
+	else
+		TEST_PASSED(1);
+	separator();
+
+	/* test 2: klist0_prepend() */
+	TEST_DECLARE(2, "klist0_prepend()");
+	klist0_prepend(&_test.the_list, &_node[1].the_list);
+	dump_node("_test", _test.the_list);
+	dump_node("_node[0]", _node[0].the_list);
+	dump_node("_node[1]", _node[1].the_list);
+	dump_node("_node[2]", _node[2].the_list);
+	if (_node[0].the_list.next != &_test.the_list ||
+	    _node[1].the_list.next != &_node[2].the_list ||
+	    _test.the_list.next != &_node[1].the_list ||
+	    _test.the_list.prev != &_node[0].the_list)
+		TEST_FAILURE(2);
+	else
+		TEST_PASSED(2);
+	separator();
+
+	/* test 3: klist0_unlink() */
+	TEST_DECLARE(3, "klist0_unlink()");
+	klist0_unlink(&_test.the_list);
+	dump_node("_test", _test.the_list);
+	dump_node("_node[0]", _node[0].the_list);
+	dump_node("_node[1]", _node[1].the_list);
+	dump_node("_node[2]", _node[2].the_list);
+	if (_node[0].the_list.next != &_node[1].the_list ||
+	    _node[1].the_list.next != &_node[2].the_list ||
+	    _node[0].the_list.prev != &_node[2].the_list ||
+	    _node[1].the_list.prev != &_node[0].the_list ||
+	    _test.the_list.next != &_test.the_list ||
+	    _test.the_list.prev != &_test.the_list)
+		TEST_FAILURE(3);
+	else
+		TEST_PASSED(3);
+	separator();
+
+	/* test 4: klist0_entry() */
+	TEST_DECLARE(4, "klist0_entry()");
+	printf("_test->data=%d\n",
+			klist0_entry(&_test.the_list,
+				list_test_struct,
+				the_list)->data);
+	if (klist0_entry(&_test.the_list, list_test_struct, the_list)->data !=
+			TEST_KEY)
+		TEST_FAILURE(4);
+	else
+		TEST_PASSED(4);
+	separator();
+	printf("tests failed: %d, tests passed: %d\n", TESTS_FAILED, TESTS_PASSED);
+
+	return TESTS_FAILED;
+}
+
