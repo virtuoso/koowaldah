306290577a8bf0dd202c525e82e94dff60b7093e
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2006-10-11 19:48:47 +0400

    * fix build with gcc 3.x Signed-off-by: Alexey Zaytsev <xl0@zaitsev_a.protei>

--- include/kuca.h
+++ include/kuca.h
@@ -14,7 +14,12 @@
 #include <arch/types.h>
 #include <config.h>
 
+#ifdef __builtin_offsetof
 #define offsetof(TYPE, MEMBER) __builtin_offsetof(TYPE, MEMBER)
+#else
+#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)NULL)->MEMBER)
+#endif
+
 #define __init __attribute__((section(".init")))
 
 #endif /* __KUCA_H_ */
