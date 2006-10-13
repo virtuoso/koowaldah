4a4cad168a1e46b20f626e6bf2b7ef83b45613fb
Alexander Shishkin <ash@koowaldah.org>
2006-10-13 04:18:09 +0400

    * changes in core timer layer according to scheduler changes and klist0 fixes Signed-off-by: Alexander Shishkin <ash@koowaldah.org>

--- kernel/timecore.c
+++ kernel/timecore.c
@@ -9,6 +9,7 @@
 #include <error.h>
 #include <mm.h>
 #include <thread.h>
+#include <scheduler.h>
 
 static struct klist0_node timers;
 
@@ -54,21 +55,25 @@ int register_timer(timer_fn func, u32 delay, void *data)
 
 void update_timers()
 {
-        struct klist0_node *t;
+        struct klist0_node *t, *d;
 	struct timer *timer;
 
         if (!klist0_empty(&timers)) {
+		disable_interrupts();
 
-		klist0_for_each(t, &timers) {
+		t = timers.next;
+		do {
 			timer = klist0_entry(t, struct timer, t_list);
-			if (timer->counter)
-				timer->counter--;
-			else {
+			if (!--timer->counter) {
+				d = t; t = t->next;
+				klist0_unlink(d);
 				timer->func(timer->data);
-				klist0_unlink(t);
 				memory_release(timer);
-			}
-                }
+			} else
+				t = t->next;
+                } while (t != &timers);
+
+		enable_interrupts();
         }
 }
 
@@ -76,6 +81,7 @@ static void tsleep_alarm(void *data)
 {
 	struct thread_t *thread = (struct thread_t *)data;
 
+	scheduler_enqueue(thread);
 	thread->state = THREAD_RUNNABLE;
 }
 
@@ -88,16 +94,18 @@ int tsleep(u32 delay)
 	disable_interrupts();
 
 	/* remove the thread from scheduler's candidates */
-	thread->state &= ~THREAD_RUNNABLE;
+	thread->state = 0;
+	scheduler_dequeue(thread);
+	//thread->state &= ~THREAD_RUNNABLE;
 	//thread->state = THREAD_WAIT;
 
 	/* this will wake us up after /at least/ delay ticks */
 	ret = register_timer(tsleep_alarm, delay, (void *)thread);
-	enable_interrupts();
 
 	/* quit running immediately */
 	if (!ret)
-		thread_yield();
+		scheduler_yield();
+	enable_interrupts();
 
 	return ret;
 }
