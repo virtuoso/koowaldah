14defb18ca59c7d2e22d0d03b09c041c927c47a6
Alexander Shishkin <ash@koowaldah.org>
2006-10-13 17:31:15 +0400

    * add atomic data structures and operations implementation Signed-off-by: Alexander Shishkin <ash@koowaldah.org>

---
+++ include/atomic.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2006, Alexander Shishkin
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of California, Berkeley nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __ATOMIC_H__
+#define __ATOMIC_H__
+/*
+ * Atomic types and operations
+ */
+
+/* For signed types, please submit a patch with a good reason */
+typedef struct { volatile u32 v; } atomic_u32;
+typedef struct { volatile u16 v; } atomic_u16;
+typedef struct { volatile u8 v; } atomic_u8;
+
+#include <arch/atomic.h>
+#ifndef __ARCH_ATOMIC_H__
+#error No atomic operations on your arch? Too bad.
+#endif
+
+#define ATOMIC_INIT(p) ((p)->v = 0)
+#define atomic_read(p) ((p)->v)
+
+#endif
