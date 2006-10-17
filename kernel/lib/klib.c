40b12631514b1a74142b2b40fc49f727a1bc1c6d
Alexander Shishkin <alexander.shishkin@siemens.com>
2006-10-17 14:48:02 +0400

    * coding style fixes Signed-off-by: Alexander Shishkin <alexander.shishckin@gmail.com>

--- kernel/lib/klib.c
+++ kernel/lib/klib.c
@@ -36,46 +36,47 @@
 #include <lib.h> 
 
 
-size_t memory_set(void *s, char c, size_t n){
+size_t memory_set(void *s, char c, size_t n)
+{
 	
 	size_t t = n;
-	while(t-- > 0)
+	while (t-- > 0)
 		((char *)s)[t] = (char)c;
 	return n;
 }
 
-size_t memory_copy( void *dest, void *src, size_t n )
+size_t memory_copy(void *dest, void *src, size_t n)
 {
 	size_t i;
 
-	if(dest == src )
+	if (dest == src)
 		return n;
 
-	if(dest < src)
-		for( i = 0; i < n; i++ )
+	if (dest < src)
+		for (i = 0; i < n; i++)
 			((char *)dest)[i] = ((char *)src)[i];
 	else
-		for( i = n; i > 0; i--)
+		for (i = n; i > 0; i--)
 			((char *)dest)[i-1] = ((char *)src)[i-1];
 				
 	return n;
 }
 
-size_t string_len( char *str )
+size_t string_len(char *str)
 {
-    char *p = str;
+	char *p = str;
 
-    while( '\0' != *p++ );
-    return p - str;
+	while ('\0' != *p++);
+	return p - str;
 }
 
-char *find_char( char *str, char c )
+char *find_char(char *str, char c)
 {
-    char *p = str;
+	char *p = str;
 
-    while( '\0' != *p && c != *p ) p++;
-    if( '\0' == *p ) return NULL;
+	while ('\0' != *p && c != *p) p++;
+	if ('\0' == *p) return NULL;
 
-    return p;
+	return p;
 }
 
