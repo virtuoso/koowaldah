967279652ffb535c719d4063f4a727f0e0fcb99f
Alexander Shishkin <alexander.shishkin@siemens.com>
2006-10-17 16:10:39 +0400

    * ash was there too Signed-off-by: Alexander Shishkin <alexander.shishckin@gmail.com>

--- include/lib.h
+++ include/lib.h
@@ -3,6 +3,7 @@
  * include/lib.h
  *
  * Copyright (C) 2005 Alexey Zaytsev
+ * Copyright (C) 2006 Alexander Shishkin
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
