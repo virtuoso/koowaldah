340b030a3eddb066e961e46e4cb95083c1df8bc1
Alexander Shishkin <alexander.shishckin@gmail.com>
2006-10-21 21:25:34 +0400

    * pc-kbd driver changed to be a normal character device Signed-off-by: Alexander Shishkin <alexander.shishckin@gmail.com>

--- drivers/keyboard/pc-kbd.c
+++ drivers/keyboard/pc-kbd.c
@@ -38,11 +38,14 @@
 #include <thread.h>
 #include <scheduler.h>
 #include <arch/asm.h>
+#include <device.h>
 #include "table.h"
 
 #define KEYBOARD_IRQ 1
 #define KEYBOARD_IO  (0x60)
 #define KEYBOARD_BUFSIZE 10
+
+#define PCKBD_DEV (DEV_DEVICE(13,128))
 
 /* let's have a ring buffer */
 static u8  key_buf[KEYBOARD_BUFSIZE];
@@ -119,10 +122,46 @@ int pckbd_close()
 	return 0;
 }
 
+static int pckbd_dev_open(struct file *file)
+{
+	return pckbd_open();
+}
+
+static int pckbd_dev_read(struct file *file, char *buf, off_t len)
+{
+	u16 data = pckbd_read();
+
+	buf[0] = (u8)(data >> 8);
+	buf[1] = (u8)(data & 0xff);
+
+	return 2;
+}
+
+static int pckbd_dev_close(struct file *file)
+{
+	pckbd_close();
+	return 0;
+}
+
+static struct file_operations pckbd_fops = {
+	.open = pckbd_dev_open,
+	.close = pckbd_dev_close,
+	.read = pckbd_dev_read,
+	.write = NULL
+};
+
+static struct device pckbd_dev = {
+	.d_name = {"pckbd"},
+	.d_dev = PCKBD_DEV,
+	.d_flags = DF_RDONLY | DF_CHAR,
+	.d_fops = &pckbd_fops
+};
+
 int pckbd_load()
 {
 	sleeper = NULL;
 	users = read_idx = write_idx = 0; /* although, that's .bss, isn it? */
+	register_device(&pckbd_dev);
 	return register_irq_handler(KEYBOARD_IRQ, pckbd_intr);
 }
 
