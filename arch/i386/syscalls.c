c05d46c296a6838e9c55c7ad897e62d14cd64934
Alexander Shishkin <ash@koowaldah.org>
2006-10-23 02:31:15 +0400

    * added trivial system call gate Signed-off-by: Alexander Shishkin <ash@koowaldah.org>

---
+++ arch/i386/syscalls.c
@@ -0,0 +1,65 @@
+/*
+ * arch/i386/syscalls.c
+ *
+ * Copyright (C) 2006 Alexander Shishkin
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Koowaldah developers nor the names of theyr
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE DEVELOPERS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+/*
+ * System calls
+ */
+#include <kuca.h>
+#include <textio.h>
+#include <bug.h>
+#include <sys/errno.h>
+#include <file.h>
+
+u32 sys_call_gate(u32 eax, u32 ebx, u32 ecx, u32 edx)
+{
+	kprintf("# SYSCALL %d\n", eax);
+	switch (eax) {
+		case 0:
+			kprintf("# %s\n", (char *)ebx);
+			return 0;
+
+		case 1:
+			return open((char *)ebx, (u32)ecx);
+
+		case 2:
+			return read((int)ebx, (char *)ecx, (size_t)edx);
+
+		/*case 3:
+			return tsleep(ebx);*/
+
+		default:
+			return -ENOSYS;
+			kprintf("syscall %d not implemented\n", eax);
+	}
+	return -ENOSYS;
+}
+
