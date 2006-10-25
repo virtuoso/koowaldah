3c8a88467147c98300262a16232d382dfd72a876
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2006-10-26 02:00:20 +0400

    * Add slice allocator tests and broaden the page allocator tests. Signed-off-by: Alexey Zaytsev <alexey.zaytsev@gmail.com>

--- kernel/init/tests.c
+++ kernel/init/tests.c
@@ -34,6 +34,7 @@
 #include <kuca.h>
 #include <console.h>
 #include <page_alloc.h>
+#include <slice.h>
 #include <mm_zone.h>
 #include <arch/asm.h>
 #include <arch/isr.h>
@@ -47,23 +48,26 @@
 #include <lib.h>
 #include <bug.h>
 
+struct page *pages[2048];
+
 void test_mm()
 {
 #ifdef OPT_PAGE_ALLOCATOR_TEST
 	struct page *pg[7];
 
-	kprintf("Testing the page allocator...\n");
+	int i;
 
+	kprintf("Testing the page allocator...\n");
 	print_alloc_info();
 
 	pg[1] = alloc_pages(0, 0);
-	kprintf("got page 0x%x, order %d\n", pg[1]->index, pg[1]->private.order);
+	kprintf("got page 0x%x, order %d\n", pg[1]->index * PAGE_SIZE, pg[1]->private.order);
 	print_alloc_info();
 
 	kprintf("\nTest 1 passed\n\n");
 
 	pg[2] = alloc_pages(0, 1);
-	kprintf("got page 0x%x, order %d\n", pg[2]->index, pg[2]->private.order);
+	kprintf("got page 0x%x, order %d\n", pg[2]->index * PAGE_SIZE, pg[2]->private.order);
 	print_alloc_info();
 
 	kprintf("\nTest 2 passed\n\n");
@@ -74,12 +78,12 @@ void test_mm()
 	print_alloc_info();
 	
 	pg[3] = alloc_pages(0, 3);
-	kprintf("got page 0x%x, order %d\n", pg[3]->index, pg[3]->private.order);
+	kprintf("got page 0x%x, order %d\n", pg[3]->index * PAGE_SIZE, pg[3]->private.order);
 	print_alloc_info();
 	kprintf("\nTest 3 passed\n\n");
 
 	pg[4] = alloc_pages(0, 1);
-	kprintf("got page 0x%x, order %d\n", pg[4]->index, pg[4]->private.order);
+	kprintf("got page 0x%x, order %d\n", pg[4]->index * PAGE_SIZE, pg[4]->private.order);
 	print_alloc_info();
 	kprintf("\nTest 4 passed\n\n");
 
@@ -88,12 +92,12 @@ void test_mm()
 	print_alloc_info();
 
 	pg[5] = alloc_pages(0, 4);
-	kprintf("got page 0x%x, order %d\n", pg[5]->index, pg[5]->private.order);
+	kprintf("got page 0x%x, order %d\n", pg[5]->index * PAGE_SIZE, pg[5]->private.order);
 	print_alloc_info();
 	kprintf("\nTest 5 passed\n\n");
 
 	pg[6] = alloc_pages(0, 2);
-	kprintf("got page 0x%x, order %d\n", pg[6]->index, pg[6]->private.order);
+	kprintf("got page 0x%x, order %d\n", pg[6]->index * PAGE_SIZE, pg[6]->private.order);
 	print_alloc_info();
 	kprintf("\nTest 6 passed\n\n");
 
@@ -112,8 +116,321 @@ void test_mm()
 	free_pages(pg[5]);
 	print_alloc_info();
 
+	pg[0] = alloc_pages(0, 1);
+	pg[1] = alloc_pages(0, 2);
+	pg[2] = alloc_pages(0, 3);
+	pg[3] = alloc_pages(0, 4);
+	pg[4] = alloc_pages(0, 5);
+	pg[5] = alloc_pages(0, 6);
+	
+	kprintf("Now allocating 2048 0-order pages.\n");
+
+	for (i = 0; i < 2048; i++) {
+		pages[i] = alloc_page(0);
+	}
+
+	kprintf("Done allocating 2048 0-roder pages.\n");
+
+	print_alloc_info();
+
+	kprintf("Freeing even pages\n");
+	
+	for (i = 0; i < 1048; i = i + 2) {
+		free_page(pages[i]);
+	}
+
+	kprintf("Done freeing even pages.\n");
+
+	print_alloc_info();
+
+
+
+	kprintf("pg[0] = 0x%x\npg[1] = 0x%x\npg[2] = 0x%x\npg[3] = 0x%x\n"
+		"pg[4] = 0x%x\npg[5] = 0x%x\npg[6] = 0x%x\n",
+		pg[0], pg[1], pg[2], pg[3], pg[4], pg[5], pg[6]);
+
+	kprintf("page_to_addr(pg[0]) = 0x%x\n", page_to_addr(pg[0]));
+	kprintf("page_to_addr(pg[1]) = 0x%x\n", page_to_addr(pg[1]));
+	kprintf("page_to_addr(pg[2]) = 0x%x\n", page_to_addr(pg[2]));
+	kprintf("page_to_addr(pg[3]) = 0x%x\n", page_to_addr(pg[3]));
+	kprintf("page_to_addr(pg[4]) = 0x%x\n", page_to_addr(pg[4]));
+	kprintf("page_to_addr(pg[5]) = 0x%x\n", page_to_addr(pg[5]));
+	kprintf("page_to_addr(pg[6]) = 0x%x\n", page_to_addr(pg[6]));
+
+	
+	kprintf("addr_to_page(page_to_addr(pg[0])) = 0x%x\n",
+		addr_to_page(page_to_addr(pg[0])));
+	kprintf("addr_to_page(page_to_addr(pg[1])) = 0x%x\n",
+		addr_to_page(page_to_addr(pg[1])));
+	kprintf("addr_to_page(page_to_addr(pg[2])) = 0x%x\n",
+		addr_to_page(page_to_addr(pg[2])));
+	kprintf("addr_to_page(page_to_addr(pg[3])) = 0x%x\n",
+		addr_to_page(page_to_addr(pg[3])));
+	kprintf("addr_to_page(page_to_addr(pg[4])) = 0x%x\n",
+		addr_to_page(page_to_addr(pg[4])));
+	kprintf("addr_to_page(page_to_addr(pg[5])) = 0x%x\n",
+		addr_to_page(page_to_addr(pg[5])));
+	kprintf("addr_to_page(page_to_addr(pg[6])) = 0x%x\n",
+		addr_to_page(page_to_addr(pg[6])));
+
+	free_page(pg[0]);
+	free_page(pg[1]);
+	free_page(pg[2]);
+	free_page(pg[3]);
+	free_page(pg[4]);
+	free_page(pg[5]);
+	free_page(pg[6]);
+
 	kprintf("Done.\n");
 #endif
+}
+
+u32 * slices[1024];
+void test_slice_alloc()
+{
+#ifdef OPT_TEST_SLICE
+	struct slice_pool *slice;
+	int i;
+
+	kprintf("Testing allocator with 4-byte slices: \n");
+
+	slice = slice_pool_create(0, 4);
+	kprintf("Created a fresh slice allocator\n");
+
+	slice_pool_info(slice);
+
+	kprintf("Allocating 1024 4-byte slices.\n");
+	
+	for (i = 0; i < 1024; i++) {
+		/* kprintf("Allocating object %d\n", i); */
+		slices[i] = slice_alloc(slice);
+		/* kprintf("Allocated object %d\n", i); */
+		/* lice_info(slice); */
+	}
+
+	kprintf("Done with allocating 1024 4-byte objects.\n");
+
+	slice_pool_info(slice);
+
+	
+	
+	/*
+	for (i = 0; i < 1024; i++) {
+		kprintf("object %d = 0x%x\n", i, objects[i]);
+	}
+	*/
+	
+	kprintf("Freeing even slices\n");
+	for (i = 0; i < 1024; i = i + 2) {
+		/* kprintf("Freeind object %d\n", i); */
+		slice_free(slices[i], slice);
+		/* kprintf("Done freeing object %d\n", i);*/
+		/* slice_pool_info(slice); */
+	}
+	
+	kprintf("Done freeing even pages.\n");
+	slice_pool_info(slice);
+
+	kprintf("Allocating even slices back.\n");
+
+	for (i = 0; i < 1024; i = i + 2) {
+		slices[i] = slice_alloc(slice);
+	}
+
+	kprintf("Done allocating even slices back.\n");
+
+	slice_pool_info(slice);
+
+	kprintf("Freeing odd slices.\n");
+
+	for (i = 1; i < 1024; i = i + 2) {
+		slice_free(slices[i], slice);
+	}
+
+	kprintf("Done freeing odd slices.\n");
+
+	slice_pool_info(slice);
+
+	kprintf("Freeing even slices.\n");
+
+	for (i = 0; i < 1024; i = i + 2) {
+		slice_free(slices[i], slice);
+	}
+
+	kprintf("Done freeing even slices.\n");
+
+	slice_pool_info(slice);
+
+	kprintf("Recycling slice pool.\n");
+
+	slice_pool_recycle(slice);
+
+	kprintf("Done recycling slice pool.\n");
+
+	slice_pool_info(slice);
+		
+
+	kprintf("Done with testing slice allocator on 4-byte slices.\n");
+	
+	kprintf("Testing allocator with 40-byte slices: \n");
+
+	slice = slice_pool_create(0, 40);
+	kprintf("Created a fresh slice allocator\n");
+
+	slice_pool_info(slice);
+
+	kprintf("Allocating 1024 40-byte slices.\n");
+	
+	for (i = 0; i < 1024; i++) {
+		/* kprintf("Allocating object %d\n", i); */
+		slices[i] = slice_alloc(slice);
+		/* kprintf("Allocated object %d\n", i); */
+		/* lice_info(slice); */
+	}
+
+	kprintf("Done with allocating 1024 40-byte objects.\n");
+
+	slice_pool_info(slice);
+
+	
+	
+	/*
+	for (i = 0; i < 1024; i++) {
+		kprintf("object %d = 0x%x\n", i, objects[i]);
+	}
+	*/
+	
+	kprintf("Freeing even slices\n");
+	for (i = 0; i < 1024; i = i + 2) {
+		/* kprintf("Freeind object %d\n", i); */
+		slice_free(slices[i], slice);
+		/* kprintf("Done freeing object %d\n", i);*/
+		/* slice_pool_info(slice); */
+	}
+	
+	kprintf("Done freeing even pages.\n");
+	slice_pool_info(slice);
+
+	kprintf("Allocating even slices back.\n");
+
+	for (i = 0; i < 1024; i = i + 2) {
+		slices[i] = slice_alloc(slice);
+	}
+
+	kprintf("Done allocating even slices back.\n");
+
+	slice_pool_info(slice);
+
+	kprintf("Freeing odd slices.\n");
+
+	for (i = 1; i < 1024; i = i + 2) {
+		slice_free(slices[i], slice);
+	}
+
+	kprintf("Done freeing odd slices.\n");
+
+	slice_pool_info(slice);
+
+	kprintf("Freeing even slices.\n");
+
+	for (i = 0; i < 1024; i = i + 2) {
+		slice_free(slices[i], slice);
+	}
+
+	kprintf("Done freeing even slices.\n");
+
+	slice_pool_info(slice);
+
+	kprintf("Recycling slice pool.\n");
+
+	slice_pool_recycle(slice);
+
+	kprintf("Done recycling slice pool.\n");
+
+	slice_pool_info(slice);
+
+	kprintf("Done with testing slice allocator on 40-byte slices.\n");
+	
+	kprintf("Testing allocator with 200-byte slices: \n");
+
+	slice = slice_pool_create(0, 200);
+	kprintf("Created a fresh slice allocator\n");
+
+	slice_pool_info(slice);
+
+	kprintf("Allocating 1024 200-byte slices.\n");
+	
+	for (i = 0; i < 1024; i++) {
+		/* kprintf("Allocating object %d\n", i); */
+		slices[i] = slice_alloc(slice);
+		/* kprintf("Allocated object %d\n", i); */
+		/* lice_info(slice); */
+	}
+
+	kprintf("Done with allocating 1024 200-byte objects.\n");
+
+	slice_pool_info(slice);
+
+	
+	
+	/*
+	for (i = 0; i < 1024; i++) {
+		kprintf("object %d = 0x%x\n", i, objects[i]);
+	}
+	*/
+	
+	kprintf("Freeing even slices\n");
+	for (i = 0; i < 1024; i = i + 2) {
+		/* kprintf("Freeind object %d\n", i); */
+		slice_free(slices[i], slice);
+		/* kprintf("Done freeing object %d\n", i);*/
+		/* slice_pool_info(slice); */
+	}
+	
+	kprintf("Done freeing even pages.\n");
+	slice_pool_info(slice);
+
+	kprintf("Allocating even slices back.\n");
+
+	for (i = 0; i < 1024; i = i + 2) {
+		slices[i] = slice_alloc(slice);
+	}
+
+	kprintf("Done allocating even slices back.\n");
+
+	slice_pool_info(slice);
+
+	kprintf("Freeing odd slices.\n");
+
+	for (i = 1; i < 1024; i = i + 2) {
+		slice_free(slices[i], slice);
+	}
+
+	kprintf("Done freeing odd slices.\n");
+
+	slice_pool_info(slice);
+
+	kprintf("Freeing even slices.\n");
+
+	for (i = 0; i < 1024; i = i + 2) {
+		slice_free(slices[i], slice);
+	}
+
+	kprintf("Done freeing even slices.\n");
+
+	slice_pool_info(slice);
+
+	kprintf("Recycling slice pool.\n");
+
+	slice_pool_recycle(slice);
+
+	kprintf("Done recycling slice pool.\n");
+
+	slice_pool_info(slice);
+
+	kprintf("Done with testing slice allocator on 200-byte slices.\n");
+
+#endif /* OPT_TEST_SLICE */
 }
 
 void test_klist()
@@ -383,6 +700,7 @@ void test_bug()
 void run_tests()
 {
 	test_mm();
+	test_slice_alloc();
 	test_klist();
 	test_threads();
 	test_pckbd();
