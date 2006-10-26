f7aaafdf6ea918cbfc287fe24a35d535ab16b2d0
Alexander Shishkin <alexander.shishckin@gmail.com>
2006-10-27 03:59:24 +0400

    * i386/asm.c: everything is rewritten and moved to asm.h Signed-off-by: Alexander Shishkin <alexander.shishckin@gmail.com>

--- arch/i386/asm.c
+++ arch/i386/asm.c
@@ -35,37 +35,6 @@
 #include <kuca.h>
 #include <arch/asm.h>
 
-
-/* Control Register operations */
-
-/* CR0 */
-inline u32 read_cr0()
-{
-        u32 result;
-        asm("mov %%cr0, %0": "=r"(result));
-        return result;
-}
-
-inline void write_cr0(u32 value)
-{
-        asm("mov %0, %%cr0"::"r"(value));
-}
-
-/* CR3 */
-inline u32 read_cr3()
-{
-        u32 result;
-        asm("mov %%cr3, %0": "=r"(result));
-        return result;
-}
-
-inline void write_cr3(u32 value)
-{
-        asm("mov %0, %%cr3"::"r"(value));
-}
-
-
-
 /* IO operations */
 
 inline u8 io_port_in(u16 port)
@@ -78,18 +47,5 @@ inline u8 io_port_in(u16 port)
 inline void io_port_out(u16 port, u8 data)
 {
 	asm("out %%al, %%dx" : :"a" (data), "d" (port));
-}
-
-/* Misc */
-
-inline u32 read_eflags()
-{
-	u32 result;
-	
-	asm volatile ("pushf");
-	asm volatile ("pop %%eax" :);
-	asm volatile ("movl %%eax, %0":"=r"(result));
-
-	return result;
 }
 
