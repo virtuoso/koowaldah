fd60a9c8fd19c61de0875e496239ec509d46ee4a
Alexander Shishkin <alexander.shishckin@gmail.com>
2006-10-27 04:04:32 +0400

    * i386/paging.c: rewritten from scratch, implemented minimal mappings So that somebody stops scratching the butt and fixing BSDL text and starts doing something useful. Signed-off-by: Alexander Shishkin <alexander.shishckin@gmail.com>

--- arch/i386/paging.c
+++ arch/i386/paging.c
@@ -2,7 +2,7 @@
 /*
  * arch/i386/paging.c
  *
- * Copyright (C) 2005 Alexey Zaytsev
+ * Copyright (C) 2006 Alexander Shishkin
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,44 +32,129 @@
  */
 
 #include <kuca.h>
-#include <arch/asm.h> 
+#include <arch/asm.h>
+#include <page_alloc.h>
+#include <textio.h>
 
-extern u8 page_directory;
-extern u8 page_table;
+struct mapping root_map;
 
-int paging_init()
+/* these are calculated in bootmem_zone.c */
+u32 phys_mem;
+u32 phys_pgs;
+
+u32 log2(u32 n)
 {
-#if 0
-	u32 address=0;
-	int i;
-
-	u32 * page_table_pointer = (u32 *) &page_table;
-	u32 * page_directory_pointer = (u32 *) &page_directory;
-
-//	kprintf("page_dir = %x, page_table = %x\n", page_table_pointer, page_directory_pointer);
-	
-	/* map the first 4 MB */
-	for(i=0; i<1024; i++){
-		page_table_pointer[i] = address | 3; /* 011 = supervisor level, read/write, present */
-		address = address + 4096; 
-	}
-
-	/* Fill the first entry in the page dir and mark others as absent */
-	page_directory_pointer[0] = ( u32) page_table_pointer | 3;
-
-	for(i=1; i<1024; i++){
-		page_directory_pointer[i] = 0 | 2; /* 010 = supervisor level, read/write, not present */
-	};
-
-//	kprintf("Enabling paging.\n");
-	
-	write_cr3((u32) page_directory_pointer);
-//	kprintf("write_cr3.\n");
-	write_cr0(read_cr0() | 0x80000000);
-//	kprintf("write cr0\n");
-		
-#endif
-	return 0;		
+	u32 l = 1;
+	while ((l < 32) && (n >> l)) l++;
+	return l - 1;
 }
 
+/*
+ * Initialize a flat 1:1 mapping
+ */
+static void init_rootmap()
+{
+	u32 pgaddr = 0;
+	int d, t;
+	u32 tablesz;
+	u32 *pgtable;
+	u32 *pgdir;
+
+	tablesz = phys_pgs / PGT_ENTRIES;
+	root_map.m_pgdir =
+	pgdir = get_pages(0, 0); /* just one page for pgdir */
+
+	root_map.m_pgtable =
+	pgtable = get_pages(0, log2(tablesz));
+
+	for (d = 0; d < tablesz; d++) {
+		for (t = 0; t < PGT_ENTRIES; t++) {
+			pgtable[t] = (pgaddr++ << PAGE_SHIFT) |
+				PTF_PRESENT | PTF_RW;
+		}
+		pgdir[d] = (u32)pgtable | PTF_PRESENT | PTF_RW;
+		pgtable += PAGE_SIZE / PTE_BYTES;
+	}
+
+	for (; d < PGD_ENTRIES; d++)
+		pgdir[d] = 0 | PTF_USER | PTF_RW;
+}
+
+void copy_map(struct mapping *dst, struct mapping *map)
+{
+	u32 *src;
+	u32 *pgtable;
+	int d, t;
+
+	dst->m_pgdir = get_pages(0, 0);
+	dst->m_pgtable = pgtable = get_pages(0, log2(phys_pgs/PGT_ENTRIES));
+	for (d = 0; d < PGD_ENTRIES; d++) {
+		/* copy flags as-is */
+		dst->m_pgdir[d] = map->m_pgdir[d] & PAGE_MASK;
+		dst->m_pgdir[d] |= (u32)pgtable;
+		src = (u32 *)(map->m_pgdir[d] & NOPAGE_MASK);
+
+		if (dst->m_pgdir[d] & PTF_PRESENT) {
+			/* copy all ptes */
+			for (t = 0; t < PGT_ENTRIES; t++)
+				pgtable[t] = src[t];
+
+			pgtable += PAGE_SIZE / PTE_BYTES;
+		}
+	}
+}
+
+void init_user_map(struct mapping *map)
+{
+	int d, t;
+	u32 *pgtable;
+	u32 pgaddr = 0x00000000;
+
+	map->m_pgdir = get_pages(0, 0);
+	map->m_pgtable = pgtable = get_pages(0, 
+			log2((phys_pgs)/PGT_ENTRIES));
+
+	for (d = 0; d < 8; d++) {
+		for (t = 0; t < PGT_ENTRIES; t++) {
+			pgtable[t] = (pgaddr++ << PAGE_SHIFT) |
+				PTF_PRESENT | PTF_RW;
+		}
+		map->m_pgdir[d] = (u32)pgtable |
+			PTF_PRESENT | PTF_RW;
+		pgtable += PAGE_SIZE / PTE_BYTES;
+	}
+
+	/* skip to 1GB */
+	for (; d < 0x100; d++)
+		map->m_pgdir[d] = 0;
+
+	for (; d < 0x108; d++) {
+		for (t = 0; t < PGT_ENTRIES; t++) {
+			pgtable[t] = (pgaddr++ << PAGE_SHIFT) |
+				PTF_USER | PTF_PRESENT | PTF_RW;
+		}
+		map->m_pgdir[d] = (u32)pgtable |
+			PTF_USER | PTF_PRESENT | PTF_RW;
+		pgtable += PAGE_SIZE / PTE_BYTES;
+	}
+
+	/* the rest is not present either */
+	for (; d < PGD_ENTRIES; d++)
+		map->m_pgdir[d] = 0;
+}
+
+void switch_map(struct mapping *from, struct mapping *to)
+{
+	write_cr3((u32)to->m_pgdir);
+}
+
+void __init paging_init()
+{
+	init_rootmap();
+
+	kprintf("# root pgdir=%x\n", root_map.m_pgdir);
+	kprintf("# root pgtable=%x\n", root_map.m_pgtable);
+	write_cr3((u32)root_map.m_pgdir);
+	write_cr0(read_cr0() | 0x80000000);
+}
 
