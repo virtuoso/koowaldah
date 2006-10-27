5aedef3db3eba6f1e9329ce604d04e52355a0eae
Alexander Shishkin <ash@koowaldah.org>
2006-10-27 04:08:53 +0400

    * i386/asm.h: reimplemented everything, added paging-related functions Yes, it was totally no good. Like:
    inline u32 read_cr0();
    
    Who on earth should inline that? Not me.
    Signed-off-by: Alexander Shishkin <ash@koowaldah.org>

--- include/i386/asm.h
+++ include/i386/asm.h
@@ -2,7 +2,7 @@
 /*
  * include/i386/asm.h 
  *
- * Copyright (C) 2005 Alexey Zaytsev
+ * Copyright (C) 2006 Alexander Shishkin
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -34,19 +34,149 @@
 #ifndef __ARCH_ASM_H__
 #define __ARCH_ASM_H__
 
+/* allow kernel to consume 1/KERN_ALLOWANCE of available physical memory */
+#define KERN_ALLOWANCE 8
+
+/* page size, bits, mask, unmask */
 #define PAGE_SHIFT 12
 #define PAGE_SIZE (1UL << PAGE_SHIFT)
+#define PAGE_MASK (PAGE_SIZE - 1)
+#define NOPAGE_MASK ~(PAGE_MASK)
 
-inline u32 read_cr0();
-inline void write_cr0(u32 value);
+/* page translation constants */
+#define PTE_BITS  32
+#define PTE_BYTES  (PTE_BITS/8)
+#define PGD_SHIFT 22
+#define PGT_SHIFT 12
+#define PGD_ENTRIES (1 << (PTE_BITS - PGD_SHIFT))
+#define PGT_ENTRIES (1 << (PGD_SHIFT - PGT_SHIFT))
+#define PGD_MASK 0xffc00000
+#define PGT_MASK 0x003ff000
 
-inline u32 read_cr3();
-inline void write_cr3(u32 value);
+/* page table flags */
+#define PTF_PRESENT  (1 << 0)
+#define PTF_RW       (1 << 1)
+#define PTF_USER     (1 << 2)
+#define PTF_PWT      (1 << 3)
+#define PTF_PCD      (1 << 4)
+#define PTF_ACCESSED (1 << 5)
+#define PTF_DIRTY    (1 << 6)
+/* more to come as needed */
+
+/* memory mapping/page directory */
+struct mapping {
+	u32 *m_pgdir;
+	u32 *m_pgtable;
+};
+
+extern struct mapping root_map;
+
+/* read/write cpu control registers */
+#define READ_REG(reg, REG) \
+static inline unsigned long read_ ##reg() \
+{ \
+	unsigned long r; \
+	__asm__ __volatile__("movl " REG ", %0" : "=r"(r)); \
+	return r; \
+}
+
+#define WRITE_REG(reg, REG) \
+static inline void write_ ##reg(unsigned long v) \
+{ \
+	__asm__ __volatile__("movl %0, " REG : : "r"(v)); \
+}
+
+READ_REG(cr0, "%%cr0")
+READ_REG(cr2, "%%cr2")
+READ_REG(cr3, "%%cr3")
+WRITE_REG(cr0, "%%cr0")
+WRITE_REG(cr2, "%%cr2")
+WRITE_REG(cr3, "%%cr3")
+
+/*
+ * Lookup the virtual __page__ address in the directory.
+ * No checks are performed as to verify presence of a page;
+ * in case of absence, page fault will raise.
+ */
+static inline u32 __virt2physpg(u32 pgaddr)
+{
+	u32 ret;
+
+	__asm__ __volatile__(
+			"movl %1, %%eax\n"
+			"andl $0xffc00000, %%eax\n"
+			"shrl $20, %%eax\n"
+			"movl %%cr3, %%ebx\n" /* page dir */
+			"addl %%ebx, %%eax\n"
+			"movl (%%eax), %%eax\n"
+			"andl $0xfffff000, %%eax\n"
+			"movl %%eax, %%ecx\n" /* page table */
+			"movl %1, %%eax\n"
+			"andl $0x003ff000, %%eax\n"
+			"shrl $10, %%eax\n"
+			"lea (%%eax, %%ecx, 1), %%eax\n"
+			"movl (%%eax), %%eax\n"
+			"andl $0xfffff000, %%eax\n"
+			"movl %%eax, %0"
+			: "=r"(ret) : "m"(pgaddr)
+		);
+	return ret;
+}
+
+/*
+ * Lookup the virtual address in the directory.
+ * No checks are performed as to verify presence of a page;
+ * in case of absence, page fault will raise.
+ */
+static inline u32 __virt2phys(u32 addr)
+{
+	u32 ret;
+
+	__asm__ __volatile__(
+			"movl %1, %%eax\n"
+			"andl $0xffc00000, %%eax\n"
+			"shrl $20, %%eax\n"
+			"movl %%cr3, %%ebx\n" /* page dir */
+			"addl %%ebx, %%eax\n"
+			"movl (%%eax), %%eax\n"
+			"andl $0xfffff000, %%eax\n"
+			"movl %%eax, %%ecx\n" /* page table */
+			"movl %1, %%eax\n"
+			"andl $0x003ff000, %%eax\n"
+			"shrl $10, %%eax\n"
+			"lea (%%eax, %%ecx, 1), %%eax\n"
+			"movl (%%eax), %%edx\n"
+			"andl $0xfffff000, %%edx\n"
+			"movl %1, %%eax\n"
+			"andl $0x00000fff, %%eax\n"
+			"orl %%edx, %%eax\n"
+			"movl %%eax, %0"
+			: "=r"(ret) : "m"(addr)
+		);
+	return ret;
+}
+
+void copy_map(struct mapping *dst, struct mapping *map);
+void init_user_map(struct mapping *map);
+void switch_map(struct mapping *from, struct mapping *to);
 
 inline u8 io_port_in(u16 port);
 
 inline void io_port_out(u16 port, u8 data);
 
-inline u32 read_eflags(); 
+/* read/write cpu flags */
+static inline u32 read_eflags()
+{
+	u32 f;
+	__asm__ __volatile__("pushfl; popl %0" : "=r"(f));
+	return f;
+}
+
+static inline u32 write_eflags()
+{
+	u32 f;
+	__asm__ __volatile__("pushl %0; popfl" : : "r"(f));
+	return f;
+}
 
 #endif /* __ARCH_ASM_H__ */
