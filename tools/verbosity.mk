c79611ccd792148b99c931bf18aba6d53e9cb863
Alexander Shishkin <alexander.shishckin@gmail.com>
2006-12-04 22:59:03 +0300

    tools/verbosity.mk: little rewrite to fix collecting exit codes Signed-off-by: Alexander Shishkin <alexander.shishckin@gmail.com>

--- tools/verbosity.mk
+++ tools/verbosity.mk
@@ -2,21 +2,36 @@
 # -----------------------------------
 VERB ?= full
 
+ifeq ($(VERB),brief)
+  FAILURE = echo "FAILED"
+  SUCCESS = echo "OK"
+else
+  FAILURE = true
+  SUCCESS = true
+endif
+
+EXEC =                                            \
+	$1;                                       \
+	if [ "$$?" != "0" ]; then                 \
+		$(FAILURE);                       \
+		false;                            \
+	else                                      \
+		$(SUCCESS);                       \
+	fi
+
 OUTPUT =                                          \
 	case "$(VERB)" in                         \
 		full)                             \
 			echo "  $1";              \
+			$(call EXEC,$1);          \
 			;;                        \
 		brief)                            \
 			echo -n "  $2";           \
-			if [ "$$?" != "0" ]; then \
-				echo "FAILED";    \
-			else                      \
-				echo "OK";        \
-			fi                        \
+			$(call EXEC,$1);          \
 			;;                        \
 		vim)                              \
 			echo "  $2";              \
+			$(call EXEC,$1);          \
 			;;                        \
 	esac
 
