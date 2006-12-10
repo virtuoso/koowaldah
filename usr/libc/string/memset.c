02744b37a9683e9880b62e01201d727b9febf7a3
Alexander Shishkin <alexander.shishckin@gmail.com>
2006-12-10 15:13:06 +0300

    libc/string/memset.c: memory_set() -> memset() Signed-off-by: Alexander Shishkin <alexander.shishckin@gmail.com>

--- usr/libc/string/memset.c
+++ usr/libc/string/memset.c
@@ -34,12 +34,13 @@
 #include <assert.h>
 #include <string.h>
 
-size_t memory_set(void *s, char c, size_t n)
+void *memset(void *s, int c, size_t n)
 {
-        
         size_t t = n;
+
         while (t-- > 0)
                 ((char *)s)[t] = (char)c;
-        return n;
+
+        return s;
 }
 
