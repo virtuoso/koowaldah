84fe7c4f8f1a22c68eb12ea5dabd695ac2c42b3a
Alexander Shishkin <alexander.shishckin@gmail.com>
2006-12-11 00:15:03 +0300

    libs/include/syscalls.h: added sys_getpid() And accidentally missing sys_yield() proto. Signed-off-by: Alexander Shishkin <alexander.shishckin@gmail.com>

--- usr/libc/include/syscalls.h
+++ usr/libc/include/syscalls.h
@@ -42,6 +42,7 @@
 #define SYS_tsleep 5
 #define SYS_fork 6
 #define SYS_yield 7
+#define SYS_getpid 8
 
 int sys_debug(char *str);
 int sys_open(char *name, unsigned int mode);
@@ -50,4 +51,6 @@ int sys_read(int fd, char *buf, unsigned int len);
 int sys_write(int fd, char *buf, unsigned int len);
 int sys_tsleep(unsigned int t);
 int sys_fork();
+int sys_yield();
+int sys_getpid();
 
