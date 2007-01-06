12a1697a20de36e52e60ca329e25b97cf60f346e
Alexander Shishkin <ash@koowaldah.org>
2007-01-07 01:01:25 +0300

    i386/paging.c: removed static initialization of userspace mappings This is done in favor of dynamic, architecture-independent mem_areas. Signed-off-by: Alexander Shishkin <ash@koowaldah.org>

--- arch/i386/paging.c
+++ arch/i386/paging.c
@@ -32,9 +32,11 @@
  */
 
 #include <koowaldah.h>
+#include <lib.h>
 #include <i386/asm.h>
 #include <page_alloc.h>
 #include <mm_zone.h>
+#include <mm.h>
 #include <textio.h>
 #include <error.h>
 
@@ -44,15 +46,9 @@ struct mapping root_map;
 u32 phys_mem;
 u32 phys_pgs;
 
-u32 log2(u32 n)
-{
-	u32 l = 1;
-	while ((l < 32) && (n >> l)) l++;
-	return l - 1;
-}
-
 /*
  * Initialize a flat 1:1 mapping
+ * which is stored in global root_map
  */
 static void init_rootmap()
 {
@@ -73,7 +69,7 @@ static void init_rootmap()
 		for (t = 0; t < PGT_ENTRIES; t++)
 			pgtable[t] = PTE_DESC(pgaddr++, PTF_PRESENT | PTF_RW);
 
-		pgdir[d] = (u32)pgtable | PTF_PRESENT | PTF_RW;
+		pgdir[d] = (u32)pgtable | PTF_PRESENT | PTF_RW | PTF_GLOBAL;
 		pgtable += PAGE_SIZE / PTE_BYTES;
 	}
 
@@ -81,12 +77,16 @@ static void init_rootmap()
 		pgdir[d] = 0 | PTF_USER | PTF_RW;
 }
 
-void copy_map(struct mapping *dst, struct mapping *map)
+/*
+ * Copy existing mapping into a newly created one
+ */
+void clone_map(struct mapping *dst, struct mapping *map)
 {
 	u32 *src;
 	u32 *pgtable;
 	int d, t;
 
+	dst->m_nmma = 0;
 	dst->m_pgdir = get_pages(0, 0);
 	dst->m_pgtable = pgtable = get_pages(0, log2(phys_pgs/PGT_ENTRIES));
 	for (d = 0; d < PGD_ENTRIES; d++) {
@@ -106,6 +106,19 @@ void copy_map(struct mapping *dst, struct mapping *map)
 	}
 }
 
+void free_map(struct mapping *map)
+{
+	put_pages((void *)map->m_pgtable);
+	put_pages((void *)map->m_pgdir);
+}
+
+/*
+ * Add a page to a mapping
+ * @map   -- the mapping
+ * @virt  -- virtual address inside the mapping (page aligned)
+ * @phys  -- physical address of the page
+ * @flags -- read/write markers
+ */
 void map_page(struct mapping *map, u32 virt, u32 phys, u16 flags)
 {
 	u32 *pgtable = (u32 *)(map->m_pgdir[PGDIDX(virt)] & NOPAGE_MASK);
@@ -115,6 +128,21 @@ void map_page(struct mapping *map, u32 virt, u32 phys, u16 flags)
 		phys | PTF_PRESENT | PTF_USER | flags;
 }
 
+void unmap_page(struct mapping *map, u32 virt)
+{
+	u32 *pgtable = (u32 *)(map->m_pgdir[PGDIDX(virt)] & NOPAGE_MASK);
+
+	pgtable[PGTIDX(virt)] = 0;
+}
+
+/*
+ * Add multiple pages to a mapping
+ * @map   -- the mapping
+ * @virt  -- virtual address inside the mapping (page aligned)
+ * @phys  -- physical address of the page
+ * @n     -- amount of pages to be mapped
+ * @flags -- read/write markers
+ */
 void map_pages(struct mapping *map, u32 virt, u32 phys, u32 n, u16 flags)
 {
 	u32 i;
@@ -124,39 +152,11 @@ void map_pages(struct mapping *map, u32 virt, u32 phys, u32 n, u16 flags)
 }
 
 /*
- * Initialize a mapping for user task
- * @map -- mapping to be tweaked
- * @cp  -- amount of requested code pages
- * @dp  -- amount of requested data pages
- * Note that this is always called when zones are already set up
+ * Switch from one memory mapping to another
+ * Part of context switch
+ * @from -- current mapping (to be removed)
+ * @to   -- new memory mapping
  */
-int init_user_map(struct mapping *map, u32 cp, u32 dp)
-{
-	u32 pgaddr;
-
-	/* explicitly copy the root mapping */
-	copy_map(map, &root_map);
-
-	pgaddr = (u32)get_pages(ZONE_USER, log2(cp));
-	if (!pgaddr)
-		return -ENOMEM;
-
-	/*kprintf("### first user code page: %x\n", pgaddr);*/
-	map_pages(map, USERMEM_VIRT, pgaddr, cp, PTF_RW);
-
-	pgaddr = (u32)get_pages(ZONE_USER, log2(dp));
-	if (!pgaddr)
-		return -ENOMEM;
-
-	/*kprintf("### first user data page: %x\n", pgaddr);*/
-	map_pages(map, USERMEM_VIRT + cp * PAGE_SIZE, pgaddr, dp, PTF_RW);
-
-	map->m_cp = cp;
-	map->m_dp = dp;
-
-	return 0;
-}
-
 void switch_map(struct mapping *from, struct mapping *to)
 {
 	write_cr3((u32)to->m_pgdir);
