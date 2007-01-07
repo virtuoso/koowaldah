01e712fdd80b1a05453c7fd6c5044ac179572369
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2007-01-07 15:37:41 +0300

    [PATCH] Move sys/types.h to uabi
    Signed-off-by: Alexey Zaytsev <alexey.zaytsev@gmail.com>
    Signed-off-by: Alexander Shishkin <alexander.shishckin@gmail.com>

--- include/uabi/i386/types.h
+++ include/uabi/i386/types.h
@@ -34,17 +34,17 @@
 #ifndef __ARCH_TYPES_H__
 #define __ARCH_TYPES_H__
 
-typedef signed char s8;
-typedef unsigned char u8;
+typedef signed char __s8;
+typedef unsigned char __u8;
 
-typedef signed short s16;
-typedef unsigned short u16;
+typedef signed short __s16;
+typedef unsigned short __u16;
 
-typedef signed int s32;
-typedef unsigned int u32;
+typedef signed int __s32;
+typedef unsigned int __u32;
 
-typedef signed long long s64;
-typedef unsigned long long u64;
-
+typedef signed long long __s64;
+typedef unsigned long long __u64;
 
 #endif /* __ARCH_TYPES_H__ */
+
