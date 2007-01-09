345a5f9c843c38c19a162e969ff764c1577743a5
Alexander Shishkin <alexander.shishckin@gmail.com>
2007-01-10 01:46:24 +0300

    include/message.h: added thread_queue to message struct so that sender can sleep if he chooses to. Signed-off-by: Alexander Shishkin <alexander.shishckin@gmail.com>

--- include/message.h
+++ include/message.h
@@ -37,23 +37,17 @@
 
 #include <koowaldah.h>
 #include <klist0.h>
+#include <khui/message.h>
+#include <thread.h>
 
 struct message {
-	struct klist0_node m_mbox;
-	pid_t              m_sender;
-	u32                m_flags;
-	char               *m_buf;
-	size_t             m_len;
+	struct klist0_node  m_mbox;   /* thread's message list link */
+	pid_t               m_sender; /* sender's pid               */
+	u32                 m_flags;  /* message's flags            */
+	char                *m_buf;   /* message buffer             */
+	size_t              m_len;    /* message length             */
+	struct thread_queue m_tq;     /* thread queue to sleep on   */
 };
-
-/*
- * Message flags
- */
-#define MF_COPY     (1 << 0) /* simple copy */
-#define MF_REMAP    (1 << 1) /* page remapping */
-#define MF_BLOCK    (1 << 2) /* block sender */
-#define MF_NOTIFY   (1 << 3) /* notify recipient */
-#define MF_GRANU    (1 << 4) /* page granularity */
 
 void __init messaging_init();
 int msg_send(pid_t recp, char *buf, size_t len, u32 flags);
