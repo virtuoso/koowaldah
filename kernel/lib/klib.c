d15fc91c5c52c1646057e269f13d35db68653244
Alexander Shishkin <ash@koowaldah.org>
2007-06-13 23:46:53 +0400

    lib: add strtoul() implementation Signed-off-by: Alexander Shishkin <ash@koowaldah.org>

--- kernel/lib/klib.c
+++ kernel/lib/klib.c
@@ -62,3 +62,61 @@ size_t memory_copy(void *dest, void *src, size_t n)
 	return n;
 }
 
+/* nicked from NetBSD C library 'strtoull' */
+u32 strtoul(char *nptr)
+{
+	char *s;
+	u32 acc, cutoff;
+	int c, base = 0;
+	int neg, any, cutlim;
+
+	s = nptr;
+	do {
+		c = (unsigned char) *s++;
+	} while (isspace(c));
+	if (c == '-') {
+		neg = 1;
+		c = *s++;
+	} else {
+		neg = 0;
+		if (c == '+')
+			c = *s++;
+	}
+	if ((base == 0 || base == 16) &&
+			c == '0' && (*s == 'x' || *s == 'X')) {
+		c = s[1];
+		s += 2;
+		base = 16;
+	}
+	if (base == 0)
+		base = c == '0' ? 8 : 10;
+
+	cutoff = ULONG_MAX / (u32)base;
+	/* LONGLONG */
+	cutlim = (int)(ULONG_MAX % (u32)base);
+	for (acc = 0, any = 0;; c = (unsigned char) *s++) {
+		if (isdigit(c))
+			c -= '0';
+		else if (isalpha(c))
+			c -= isupper(c) ? 'A' - 10 : 'a' - 10;
+		else
+			break;
+		if (c >= base)
+			break;
+		if (any < 0)
+			continue;
+		if (acc > cutoff || (acc == cutoff && c > cutlim)) {
+			any = -1;
+			acc = ULONG_MAX;
+		} else {
+			any = 1;
+			acc *= (u32)base;
+			acc += c;
+		}
+	}
+	if (neg && any > 0)
+		acc = -acc;
+
+	return (acc);
+}
+
