9d76298585937adefd4ac4512c9854842f12d56c
Alexander Shishkin <alexander.shishckin@gmail.com>
2007-06-13 23:56:27 +0400

    extend conversion specifiers with digits So to make %08x possible at last. Signed-off-by: Alexander Shishkin <alexander.shishckin@gmail.com>

--- kernel/lib/vsnprintf.c
+++ kernel/lib/vsnprintf.c
@@ -89,10 +89,18 @@ static size_t __noprof sprint_int_unsigned(char *buf, unsigned long i, char base
 
 int __noprof vsnprintf(char *buf, size_t size, const char *format, va_list args)
 {
+	/* format string length */
 	int flen = kstrlen((char *)format) + 1;
+	/* current position on the format string */
 	char *cur = (char *)format;
+	/* previous position on the format string */
 	char *old = (char *)format;
+	/* position in the output buffer */
 	char *pos = buf;
+	int fmt_broken = 0;
+	/* %x08: 0 is the 'tag' and 8 is 'num' */
+	char fill_tag = 0;
+	int fill_num = 0;
 	/* args */
 	long a_x;
 	/*char a_c;*/
@@ -101,19 +109,35 @@ int __noprof vsnprintf(char *buf, size_t size, const char *format, va_list args)
 
 	/* parse <format> */
 	do {
+		/* look for '%' sign, copy everything before that
+		 * to output buffer */
 		cur = kstrchr(cur, '%');
 		if (NULL != cur)
 			pos += memory_copy(pos, old, cur - old);
 
+reswitch:
+		/* check for EOLN or no-more-'%'-signs and skip the '%' sign
+		 * in the format string */
 		if (NULL == cur || '\0' == *cur++)
 			break;
 
-		switch(*cur) {
+		/* now, the conversion specifiers */
+		switch (*cur) {
+			case '0'...'9':
+				if (!fill_tag)
+					fill_tag = *cur;
+				else if (!fill_num)
+					fill_num = *cur - '0';
+				else
+					fmt_broken++;
+				goto reswitch;
+
 			case 'd':
 				memory_set(sbuf, 0, 12);
 				a_x = va_arg(args, int);
 				a_x = sprint_int_signed(sbuf, a_x, 10);
 				pos += memory_copy(pos, sbuf, a_x);
+				fill_tag = fill_num = 0;
 				break;
 
 			case 'l':
@@ -121,13 +145,21 @@ int __noprof vsnprintf(char *buf, size_t size, const char *format, va_list args)
 				a_x = va_arg(args, long);
 				a_x = sprint_int_signed(sbuf, a_x, 10);
 				pos += memory_copy(pos, sbuf, a_x);
+				fill_tag = fill_num = 0;
 				break;
 
 			case 'x':
 				memory_set(sbuf, 0, 12);
 				a_x = va_arg(args, long);
 				a_x = sprint_int_unsigned(sbuf, a_x, 16);
+
+				/* pad output with fill_tag to fill_num
+				 * characters */
+				for (; fill_num - a_x > 0; fill_num--)
+					*pos++ = fill_tag;
+
 				pos += memory_copy(pos, sbuf, a_x);
+				fill_tag = fill_num = 0;
 				break;
 
 #if 0
@@ -139,10 +171,18 @@ int __noprof vsnprintf(char *buf, size_t size, const char *format, va_list args)
 
 			case 's':
 				a_s = va_arg(args, char *);
+				if (!a_s || *a_s == '\0') {
+					pos += memory_copy(pos, "<null>", 6);
+					break;
+				}
+
 				pos += memory_copy(pos, a_s, kstrlen(a_s));
+				fill_tag = fill_num = 0;
 				break;
 
 			default:
+				fmt_broken++;
+				fill_tag = fill_num = 0;
 				/* fail silently */
 				break;
 		}
