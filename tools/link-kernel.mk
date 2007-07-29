ae544866da4f2dc4e8d75ae177dc1bda17c374a4
Alexander Shishkin <ash@koowaldah.org>
2007-07-29 23:04:31 +0400

    add one more interim stage to kernel linking procedure This is necessary to get symbol addresses correct after full-sized symbol_table gets linked in. Signed-off-by: Alexander Shishkin <ash@koowaldah.org>

--- tools/link-kernel.mk
+++ tools/link-kernel.mk
@@ -11,6 +11,15 @@ $(OBJDIR)/symtab-empty.o: $(OBJDIR)/symtab-empty.c
 	grep "$@" $(OBJDIR)/OBJECTS || \
 		echo "$(OBJDIR)/symtab-empty.o" >> $(OBJDIR)/OBJECTS
 
+# * generate stage1 symbol table
+$(OBJDIR)/symtab-stage1.c: $(OBJDIR)/OBJECTS
+	sh $(PRJROOT)/tools/symtab.sh $(kos_nosyms) $@
+
+$(OBJDIR)/symtab-stage1.o: $(OBJDIR)/symtab-stage1.c
+	@$(call DO_CC,$<,$@,$(CC_FLAGS) -I$(PRJROOT)/include)
+
+	sed -i -e 's,symtab-empty,symtab-stage1,' $(OBJDIR)/OBJECTS
+
 # * generate symbol table
 $(OBJDIR)/symtab.c: $(OBJDIR)/OBJECTS
 	sh $(PRJROOT)/tools/symtab.sh $(kos_nosyms) $@
@@ -18,18 +27,22 @@ $(OBJDIR)/symtab.c: $(OBJDIR)/OBJECTS
 $(OBJDIR)/symtab.o: $(OBJDIR)/symtab.c
 	@$(call DO_CC,$<,$@,$(CC_FLAGS) -I$(PRJROOT)/include)
 
-	sed -i -e 's,symtab-empty,symtab,' $(OBJDIR)/OBJECTS
+	sed -i -e 's,symtab-stage1,symtab,' $(OBJDIR)/OBJECTS
 
 # * link an image w/o symbol table
 $(kos_nosyms): $(OBJDIR)/OBJECTS $(OBJDIR)/symtab-empty.o
 	cat $< | xargs $(LD) $(LD_FLAGS) -o $@
 
+# * link an image w/o symbol table
+$(kos_nosyms).1: $(OBJDIR)/OBJECTS $(kos_nosyms) $(OBJDIR)/symtab-stage1.o
+	cat $< | xargs $(LD) $(LD_FLAGS) -o $@
+
 # * link final image
-$(kos_final): $(kos_nosyms) $(OBJDIR)/symtab.o
+$(kos_final): $(kos_nosyms).1 $(OBJDIR)/symtab.o
 	cat $(OBJDIR)/OBJECTS | xargs $(LD) $(LD_FLAGS) -o $@
 
 link-kernel: $(kos_final)
 
-CLEAN_FILES += $(kos_final) $(kos_nosyms)
+CLEAN_FILES += $(kos_final) $(kos_nosyms) $(kos_nosyms).1
 
 .PHONY: link-kernel
