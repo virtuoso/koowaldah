045728f83bff2bccffd64204a0f26387e8c3b61a
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2009-01-15 12:34:03 +0300

    Make koowaldah build on x86_64 and clean up/rework some makefiles.
    Signed-off-by: Alexey Zaytsev <alexey.zaytsev@gmail.com>

--- tools/vars.mk
+++ tools/vars.mk
@@ -2,6 +2,10 @@
 # Various variables are set here.
 #
 
+include $(TOOLSDIR)/version.mk
+
+VERSION_STRING = $(VERMAJOR).$(VERMINOR).$(VEREXTRA)$(MK_VERSION_APPEND)
+
 ifeq ($(MK_CPU_ARCH_DUMMY),Y)
 ARCH := dummy
 endif
@@ -15,76 +19,35 @@ ifeq ($(MK_CPU_ARCH_ARM),Y)
 ARCH := arm
 endif
 
-CROSS_COMPILE  ?=
+HOSTCC	= gcc
+HOSTLD	= gcc
+CC	= $(CROSS_COMPILE)gcc
+LD	= $(CROSS_COMPILE)ld
+ASM	= $(CROSS_COMPILE)gcc
+OBJCOPY = $(CROSS_COMPILE)objcopy
+AR	= $(CROSS_COMPILE)ar
 
-ifndef HOSTCC
-HOSTCC		:= gcc
-endif
+SYS_HOSTCC_FLAGS = -Wall -g
+SYS_HOSTLD_FLAGS =
+SYS_CC_FLAGS = -Werror -Wall -ffreestanding -nostdinc -nostdlib \
+		-g -O2 -std=gnu89 -fno-strict-aliasing -funsigned-bitfields \
+		-funsigned-char -fno-asm -fno-builtin -fno-cond-mismatch \
+		-fno-force-addr -finline-limit=1200 -fno-omit-frame-pointer \
+		-DVERSION_STRING=\"$(VERSION_STRING)\" \
+		$(shell [ ! -f $(PRJROOT)/confdefs ] || cat $(PRJROOT)/confdefs)
+SYS_LD_FLAGS =
+SYS_ASM_FLAGS = -Wall -ffreestanding -nostdinc -nostdlib -g
 
-HOST_MACH := $(shell $(HOSTCC) -dumpmachine | sed -e 's,\(^[^-]\+\)-.*,\1,')
+HOST_MACH := $(shell $(HOSTCC) -dumpmachine | cut -d - -f 1)
 HOST_OS   := $(shell uname)
 
 # add per-machine and per-os defines should such exist
--include $(PRJROOT)/arch/$(ARCH)/sysdeps/$(HOST_MACH)/defs.mk
--include $(PRJROOT)/arch/$(ARCH)/sysdeps/$(HOST_OS)/defs.mk
+-include $(PRJROOT)/arch/$(ARCH)/sysdeps/$(HOST_MACH)-defs.mk
+-include $(PRJROOT)/arch/$(ARCH)/sysdeps/$(HOST_OS)-defs.mk
 
-# check or sysdeps-defined CC
-ifndef HOSTCC
-ifdef SYSDEPSHOSTCC
-HOSTCC		= $(SYSDEPSHOSTCC)
-else
-HOSTCC		= gcc
-endif
-endif
+HOSTCC_FLAGS = $(SYS_HOSTCC_FLAGS)
+HOSTLD_FLAGS = $(SYS_HOSTLD_FLAGS)
+CC_FLAGS = $(SYS_CC_FLAGS)
+LD_FLAGS = $(SYS_LD_FLAGS)
+ASM_FLAGS = $(SYS_ASM_FLAGS)
 
-# check for sysdeps-defined LD
-ifndef HOSTLD
-ifdef SYSDEPSHOSTLD
-HOSTLD		= $(SYSDEPSHOSTLD)
-else
-HOSTLD		= gcc
-endif
-endif
-
-# include sysdeps-defined flags
-HOSTCC_FLAGS	= -Wall -g $(SYSDEPSHOSTCC_FLAGS)
-HOSTLD_FLAGS	= $(SYSDEPSHOSTLD_FLAGS)
-
-CC              = $(CROSS_COMPILE)gcc
-ifndef CC_FLAGS
-CC_FLAGS        = -Werror -Wall -ffreestanding -nostdinc -nostdlib -g -O2 -std=gnu89 -fno-strict-aliasing -funsigned-bitfields -funsigned-char -fno-asm -fno-builtin -fno-cond-mismatch -fno-force-addr -finline-limit=1200 -fno-omit-frame-pointer $(shell [ ! -f $(PRJROOT)/confdefs ] || cat $(PRJROOT)/confdefs) $(SYSDEPSCC_FLAGS)
-
-# Add versioning tags
--include $(TOOLSDIR)/version.mk
-
-ifdef KOOWALDAH_VERMAJOR
-VERSION_STRING:=$(KOOWALDAH_VERMAJOR).$(KOOWALDAH_VERMINOR).$(KOOWALDAH_VEREXTRA)
-ifneq (,$(MK_VERSION_APPEND))
-VERSION_STRING:= $(subst $(VERSION_STRING),$(VERSION_STRING),$(VERSION_STRING)-$(MK_VERSION_APPEND))
-endif
-
-CC_FLAGS += \
-	-DVERMAJOR=$(KOOWALDAH_VERMAJOR) \
-	-DVERMINOR=$(KOOWALDAH_VERMINOR) \
-	-DVEREXTRA=$(KOOWALDAH_VEREXTRA) \
-	-DVERSION_STRING=\"$(VERSION_STRING)\"
-endif
-endif
-
-CC_FLAGS_KERN  := $(CC_FLAGS)
-
-LD              = $(CROSS_COMPILE)ld
-
-ifndef LD_FLAGS
-LD_FLAGS	:= $(SYSDEPSLD_FLAGS)
-endif
-
-ASM             = $(CROSS_COMPILE)gcc
-ASM_FLAGS      += -Wall -ffreestanding -nostdinc -nostdlib -g
-
-OBJCOPY         = $(CROSS_COMPILE)objcopy
-AR              = $(CROSS_COMPILE)ar
-# MAKE ?        = make
-
-export CROSS_COMPILE HOSTCC CC CC_FLAGS CC_FLAGS_KERN LD LD_FLAGS \
-       ASM ASM_FLAGS OBJCOPY AR HOSTLD HOSTCC_FLAGS HOSTLD_FLAGS
